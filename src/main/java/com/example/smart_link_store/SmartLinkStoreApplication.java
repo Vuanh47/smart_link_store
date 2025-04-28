1c1cfc53ee283b9f1806c79aa2bb639c57f6224c
Vuanh47 <daovuanh2207@gmail.com>
2025-04-28 22:09:10 +0700

    first commit

---
+++ src/main/java/com/example/smart_link_store/SmartLinkStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.smart_link_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartLinkStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartLinkStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/smart_link_store/SmartLinkStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smart_link_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartLinkStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
